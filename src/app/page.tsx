c24378afbc257caaf999f58a0ad321fad0a3d0f2
Fikri Rakala <fikrirakala@gmail.com>
2024-08-28 08:58:53 +0800

    Build the header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import dynamic from "next/dynamic";
-
-const ThemeToggle = dynamic(() => import("@/components/ThemeToggle"), {
-  ssr: false,
-});
-
 export default function Home() {
   return (
     <section className="py-24">
       <div className="container">
         <h1 className="text-3xl font-bold">Portfolio</h1>
-
-        <ThemeToggle />
       </div>
     </section>
   );
