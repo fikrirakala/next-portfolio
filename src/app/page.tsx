e716be9f49d1563a44be760aae12cc3d24f7ef58
Fikri Rakala <fikrirakala@gmail.com>
2024-08-28 21:16:04 +0800

    Add recent posts component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import IntroSection from "@/components/IntroSection";
+import RecentPosts from "@/components/RecentPosts";
 
 export default function Home() {
   return (
     <section className="pb-24 pt-40">
       <div className="container max-w-3xl">
         <IntroSection />
+        <RecentPosts />
       </div>
     </section>
   );
