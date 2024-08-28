a878d199d9be92e5de0824d0528ea6b629a667dd
Fikri Rakala <fikrirakala@gmail.com>
2024-08-29 07:21:21 +0800

    Add projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import IntroSection from "@/components/IntroSection";
 import RecentPosts from "@/components/RecentPosts";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <div className="container max-w-3xl">
         <IntroSection />
         <RecentPosts />
+        <RecentProjects />
       </div>
     </section>
   );
