43f522b97b57a8bd1050e33e82a0b2ee3e909a85
Fikri Rakala <fikrirakala@gmail.com>
2024-08-28 16:47:16 +0800

    Build posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import Posts from "@/components/Posts";
+import { getPosts } from "@/lib/posts";
+// import PostsWithSearch from "@/components/posts-with-search";
 
-export default function PostsPage() {
-  return <div>page</div>;
+export default async function PostsPage() {
+  const posts = await getPosts();
+
+  return (
+    <section className="pb-24 pt-40">
+      <div className="container max-w-3xl">
+        <h1 className="title mb-12">Posts</h1>
+
+        {/* <PostsWithSearch posts={posts} /> */}
+        <Posts posts={posts} />
+      </div>
+    </section>
+  );
 }
